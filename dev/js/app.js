5d61714e689d9e5ee0d1de0d3d41b1b0fc104690
b.husainov <bulatkhusainov@yandex.ru>
2016-03-31 14:39:43 +0300

    add slider component

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,2 +1,33 @@
 "use strict";
 
+function initSlider() {
+	var products = document.querySelectorAll(".slider-products .product");
+	var labels = document.querySelectorAll(".product-labels .label");
+
+	labels[0].classList.add("fade-in");
+	products[0].classList.add("slide-in");
+	var currentSlideIndex = 0;
+
+	var animationInterval = 3000;
+	setInterval(function () {
+		labels[currentSlideIndex].classList.remove("fade-in");
+		labels[currentSlideIndex].classList.add("fade-out");
+
+		products[currentSlideIndex].classList.remove("slide-in");
+		products[currentSlideIndex].classList.add("slide-out");
+
+		currentSlideIndex += 1;
+
+		if (currentSlideIndex + 1 > labels.length) {
+			currentSlideIndex = 0;
+		}
+
+		labels[currentSlideIndex].classList.remove("fade-out");
+		labels[currentSlideIndex].classList.add("fade-in");
+
+		products[currentSlideIndex].classList.remove("slide-out");
+		products[currentSlideIndex].classList.add("slide-in");
+	}, animationInterval);
+}
+
+window.onload = initSlider;
