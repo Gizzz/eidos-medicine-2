65c9cedd1ba0a1e2e496ddf0cdbdaf8bf4b5bec9
b.husainov <bulatkhusainov@yandex.ru>
2016-04-04 17:54:58 +0300

    add animation to numbers section

--- dev/js/app.js
+++ dev/js/app.js
@@ -8,7 +8,7 @@ function initSlider() {
 	pics[0].classList.add("slide-in");
 	var currentSlideIndex = 0;
 
-	var animationInterval = 2000;
+	var animationInterval = 5000;
 	setInterval(function () {
 		labels[currentSlideIndex].classList.remove("fade-in");
 		labels[currentSlideIndex].classList.add("fade-out");
@@ -29,4 +29,40 @@ function initSlider() {
 	}, animationInterval);
 }
 
-window.onload = initSlider;
+function initNumbersCounter() {
+	var options = {
+		useEasing : false,
+		useGrouping : true,
+		separator : ',',
+		decimal : '.',
+		prefix : '',
+		suffix : ''
+	};
+
+	var animationDuration = 1;
+
+	var counter1 = new CountUp("counter-1", 0, 1, 0, animationDuration, options);
+	counter1.start();
+
+	var counter2 = new CountUp("counter-2", 0, 9, 0, animationDuration, options);
+	counter2.start();
+
+	var counter3 = new CountUp("counter-3", 0, 5, 0, animationDuration, options);
+	counter3.start();
+
+	var counter4 = new CountUp("counter-4", 0, 100, 0, animationDuration, options);
+	counter4.start();
+
+	options.suffix = "+";
+	var counter5 = new CountUp("counter-5", 0, 5, 0, animationDuration, options);
+	options.suffix = "";
+	counter5.start();
+
+	var counter6 = new CountUp("counter-6", 0, 10, 0, animationDuration, options);
+	counter6.start();
+}
+
+window.onload = function () {
+	initSlider();
+	initNumbersCounter();
+};
